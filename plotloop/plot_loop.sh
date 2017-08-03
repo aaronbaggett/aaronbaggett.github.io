d34c5b35016c8fa5696cbfbed4159ace27340d2d
Aaron Baggett <aaronbaggett@gmail.com>
2017-08-03 07:46:32 -0500

    Update TSLA data

--- plotloop/plot_loop.sh
+++ plotloop/plot_loop.sh
@@ -2,7 +2,7 @@
 # Diamonds plot loop bash script
 
 chmod u+x plot_loop.sh &&
-cd "/Users/AB/aaronbaggett.github.io/plotloop" && 
+# cd "/Users/AB/aaronbaggett.github.io/plotloop" && 
 Rscript "/Users/AB/aaronbaggett.github.io/plotloop/print_plots.R" &&
 git add . &&
 git commit -m "Add plot loops" &&
