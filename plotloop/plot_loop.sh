80430063a3c0f6754b22f07640e3af1760d4d14e
Aaron Baggett <aaronbaggett@gmail.com>
2017-08-03 10:59:06 -0500

    Add plot loops

--- plotloop/plot_loop.sh
+++ plotloop/plot_loop.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Diamonds plot loop bash script
 
-cd "/Users/AB/aaronbaggett.github.io/plotloop" && 
-chmod u+x plot_loop.sh && ./plot_loop.sh &&
+# cd "/Users/AB/aaronbaggett.github.io/plotloop" && 
+# chmod u+x plot_loop.sh && ./plot_loop.sh &&
 Rscript "/Users/AB/aaronbaggett.github.io/plotloop/print_plots.R" &&
 git add . &&
 git commit -m "Add plot loops" &&
